81e0e7e69035c88094ac8a7f1cdd8b39dfed299f
Kjetil <kjetilah@uia.no>
2025-04-08 17:39:38 +0200

    Beginning of project

---
+++ main.py
@@ -0,0 +1,24 @@
+import os
+os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "1"
+import pygame
+
+def main():
+    print("Starting Asteroids!")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
