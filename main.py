15b54df050a3a512fa4931397b6ad9633092d203
Kjetil <kjetilah@uia.no>
2025-04-08 18:04:55 +0200

    Made window and constants file

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 import os
 os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "1"
 import pygame
+from constants import *
 
 def main():
-    print("Starting Asteroids!")
+    pygame.init
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) 
+
+    while(1):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill(black)
+        
 
 
 
+        pygame.display.flip()
 
 
 
