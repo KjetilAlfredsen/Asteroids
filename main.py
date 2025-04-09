f65b2ac74c952746e4572142561bb8a37435e5a9
Kjetil <kjetilah@uia.no>
2025-04-09 17:58:58 +0200

    Added Asteroids

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "1"
 import pygame
 from constants import *
 from player import *
+from asteroidfield import *
 
 
 
@@ -12,8 +13,18 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) 
     clock = pygame.time.Clock()
     dt = 0
+   
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Player.containers = (drawable, updatable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+
     player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
-    
+
+    asteroidfield = AsteroidField()
 
     while(1):
         for event in pygame.event.get():
@@ -22,8 +33,9 @@ def main():
         screen.fill("black")
 
         
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+        for drawing in drawable:
+            drawing.draw(screen)
 
         pygame.display.flip()
         tickvalue = clock.tick(60)
